2cf46c7617028fdba4edba14350a5c234781fd70
Seulchan Han <paulh27@uw.edu>
2021-09-17 21:26:27 -0700

    working version

--- game.js
+++ game.js
@@ -6,23 +6,32 @@ const DisplayController = (() => {
     let closeButton = document.querySelector(".close");
     let display = document.querySelector(".container");
     let restartButton = document.querySelector(".restart");
+    let oButton = document.querySelector(".o");
+    let xButton = document.querySelector(".x");
 
-    restartButton.onclick = function () {
+    restartButton.onclick = function() {
         game.restart();
     }
-    closeButton.onclick = function () {
-        modal.style.display = "none";
+    closeButton.onclick = function() {
+        modal.style.height = "0";
+        modal.style.width="0";
         game.restart();
     }
-
     window.onclick = function (event) {
         if (event.target == modal) {
-            modal.style.display = "none";
+            modal.style.height= "0";
+            modal.style.width="0";
             game.restart();
-
         }
     }
-
+    oButton.onclick = function() {
+        Player.changeSymbol("O");
+        game.restart();
+    }
+    xButton.onclick = function() {
+        Player.changeSymbol("X");
+        game.restart();
+    }
     return { display, modal, modalContent };
 })();
 
@@ -35,15 +44,13 @@ const gameBoard = (() => {
     }
     const displayState = () => {
         for (let i = 0; i < 9; i++) {
-            DisplayController.display.children[i].innerHTML = gameArray[i];
+            DisplayController.display.children[i].firstChild.innerHTML = gameArray[i];
         }
     };
-
     const updateState = (index, symbol) => {
         gameArray[index] = symbol;
 
     };
-
     const gameWon = (symbol) => {
         if ((gameArray[0] == symbol && gameArray[3] == symbol && gameArray[6] == symbol)
             || (gameArray[1] == symbol && gameArray[4] == symbol && gameArray[7] == symbol)
@@ -59,47 +66,20 @@ const gameBoard = (() => {
             return false
         }
     }
-
     const refreshArray = () => {
         gameArray = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '];
+
+        for (let i = 0; i < 9; i++) {
+            DisplayController.display.children[i].firstChild.className = "";
+            DisplayController.display.children[i].firstChild.style.opacity = "0";
+        }
     }
-
-
-
-
     return { displayState, updateState, getArray, gameWon, refreshArray };
 })();
 
 const game = (() => {
 
     let gameEnd = false;
-
-    const playRound = (unit, symbol) => {
-        gameBoard.updateState(unit.id, symbol);
-        gameBoard.displayState();
-
-        if (!gameBoard.getArray().includes(' ')) {
-            tieGame();
-        }
-        else if (gameBoard.gameWon(symbol)) {
-            gameOver(symbol);
-        }
-        else {
-            console.log(Player.returnTurn());
-            if (!gameEnd && Player.returnTurn()) {
-                console.log("its players turn");
-                //Player.getChoice();
-            }
-            else if (!gameEnd) {
-                console.log("its computers turn");
-                Computer.getComputerChoice();
-            }
-        }
-
-
-
-
-    }
     const play = () => {
         gameBoard.displayState();
         if (Player.returnTurn()) {
@@ -108,70 +88,111 @@ const game = (() => {
         else {
             Computer.getComputerChoice();
         }
-
-
     }
+    const playRound =  (unit, symbol) => {
+        if (symbol != Player.getSymbol() ) {
+            DisplayController.display.children[unit.id].firstChild.style.transitionDelay = "500ms";
+        }
+        DisplayController.display.children[unit.id].firstChild.className = "animate";
 
+        gameBoard.updateState(unit.id, symbol);
+        gameBoard.displayState();
+        
+        if (gameBoard.gameWon(symbol)) {
+            gameOver(symbol);
+        }
+        else if (!gameBoard.getArray().includes(' ')) {
+            tieGame();
+        }
+        else {
+            if (!gameEnd && Player.returnTurn()) {
+                //Player.getChoice();
+            }
+            else if (!gameEnd) {
+                Computer.getComputerChoice();
+            }
+        }
+    }
     const tieGame = () => {
         gameEnd = true;
-        DisplayController.modal.style.display = "block";
+        DisplayController.modal.style.height = "100%";
+        DisplayController.modal.style.width = "100%";
+        DisplayController.modal.classList.add("animateModal");
         DisplayController.modalContent.innerHTML = "You Tied!";
-
     }
 
     const gameOver = (symbol) => {
-        DisplayController.modal.style.display = "block";
+        DisplayController.modal.style.height = "100%";
+        DisplayController.modal.style.width = "100%";
+        DisplayController.modal.classList.add("animateModal");
+
         if (symbol == Player.getSymbol()) {
             DisplayController.modalContent.innerHTML = "You Won!";
-
         } else {
             DisplayController.modalContent.innerHTML = "You Lost!";
         }
     }
 
     const restart = () => {
-        console.log("game restarted");
+        for (let i  = 0; i < 9 ;i++) {
+            DisplayController.display.children[i].firstChild.style.transitionDelay = "0ms";
+        }
         gameEnd = false;
         gameBoard.refreshArray();
         gameBoard.displayState();
-        Player.setTurn(true);
-        console.log(Player.returnTurn());
-        //Player.getChoice();
+        DisplayController.modal.classList.remove("animateModal");
+        if(Player.getSymbol() == "X") {
+            Player.setTurn(true);
+        } else {
+            Computer.getComputerChoice();
+        }
     }
-
     return { play, playRound, restart };
 })();
 
 const Player = (() => {
     let symbol = "X";
+    let playersTurn = true;
+
+    const changeSymbol = (letter) => {
+        if (letter == "X"){
+            symbol = "X";
+            Computer.changeSymbol("O");
+        }
+        else {
+            symbol = "O";
+            Computer.changeSymbol("X");
+        }
+    }
     const getSymbol = () => {
         return symbol;
     };
-
-    let playersTurn = true;
-    let indexChosen = 8;
-
     const setTurn = ( turnBool) => {
-        console.log(playersTurn);
         playersTurn = turnBool;
     }
     const returnTurn = () => {
         return playersTurn;
     }
-
     DisplayController.display.addEventListener("click", function (e) {
-        console.log("whyyyy");
         if (playersTurn && e.target.className == "unit" && gameBoard.getArray()[e.target.id] == ' ') {
             playersTurn = false;
             game.playRound(e.target, symbol);
         }
     })
-
-    return {  getSymbol, setTurn, returnTurn };
+    return {  getSymbol, setTurn, returnTurn, changeSymbol };
 })();
 
 const Computer = (() => {
     let symbol = "O";
+
+    const changeSymbol = (letter) => {
+        if (letter == "X"){
+            symbol = "X";
+        }
+        else {
+            symbol = "O";
+        }
+    }
     const getSymbol = () => {
         return symbol;
     }
@@ -179,17 +200,13 @@ const Computer = (() => {
         let randomIndex = Math.floor(Math.random() * 9);
         while (gameBoard.getArray()[randomIndex] != ' ') {
             randomIndex = Math.floor(Math.random() * 9);
-            console.log(randomIndex);
-
         }
         Player.setTurn(true);
         game.playRound(document.getElementById(randomIndex), symbol);
     }
 
-    return { getComputerChoice, getSymbol };
+    return { getComputerChoice, getSymbol ,changeSymbol};
 })();
-
-
 
 game.play();
 
